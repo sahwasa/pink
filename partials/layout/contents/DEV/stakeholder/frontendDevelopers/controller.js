5ea20b3d41fcce47128dcc5f556cfba6f3fb59dc
Ban Misun <jjaq88777@hanmail.net>
2016-07-18 11:11:32 +0900

    ㅠㅠ...

--- partials/layout/contents/DEV/stakeholder/frontendDevelopers/controller.js
+++ partials/layout/contents/DEV/stakeholder/frontendDevelopers/controller.js
@@ -1,17 +1,22 @@
 'use strict';
 
 define(['projectWeb'], function () {
-	
+
 	var frontendDevelopersModule = angular.module('projectWeb', ['ui.router', 'oc.lazyLoad', 'frontendDevelopersService', 'devLayoutService']);
-	
+
 	frontendDevelopersModule.controller('frontendDevelopersController', ['$scope', '$ocLazyLoad', 'frontendDevelopersService', 'devLayoutService',
 	                                      	        function($scope, $ocLazyLoad, frontendDevelopersService, devLayoutService) {
-		
+
 		$ocLazyLoad.load([
 		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.cookie.js',
 		                  'partials/common/js/jstree-v.pre1.0/_lib/jquery.hotkeys.js',
 		                  'partials/common/js/jstree-v.pre1.0/jquery.jstree.js',
-		                  
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.mCustomScrollbar.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.mousewheel.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jcarousel.min.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/jquery.usquare.js',
+                      'partials/layout/contents/DEV/stakeholder/frontendDevelopers/custom.js',
+
 		                  'AdminLTE-2.3.3/plugins/datatables/jquery.dataTables.min.css',
 		                  'AdminLTE-2.3.3/plugins/datatables/dataTables.bootstrap.css',
 		              	  'AdminLTE-2.3.3/plugins/datatables/extensions/Responsive/css/dataTables.responsive.css'
@@ -21,7 +26,7 @@ define(['projectWeb'], function () {
         	  devLayoutService.fire();
 	    });
 
-		
+
 	}]);//frontendDevelopersController.controller
-	
+
 });
